b74d20a07217f90ec1c16b1f2e74d53394600b98
Ivan Mardini <imardinig@gmail.com>
2024-08-03 15:41:23 +0200

    feat: connect-with-db

--- src/components/component/landing-page.tsx
+++ src/components/component/landing-page.tsx
@@ -2,7 +2,7 @@ import { Container } from "./container";
 import { Hero } from "./hero";
 import { OurHistory } from "./our-history";
 import { WeddingDetails } from "./wedding-details";
-import { Gallery } from "./gallery";
+import Gallery from "./gallery";
 import { RSVPForm } from "./rsvp-form";
 import { HeroParallax } from "./hero-parallax";
 import { useParallax } from "react-scroll-parallax";
