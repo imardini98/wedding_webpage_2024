9c2e5083f2d5a7c9aab4e1fbb2824f52597c0ab7
Ivan Mardini <imardinig@gmail.com>
2024-08-04 17:40:42 +0200

    feat: add i18n support

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,16 @@
+import { LandingPage } from "@/components/landing-page";
+import { getDictionary } from "@/get-dictionary";
+import { Locale } from "@/i18n-config";
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
+  return (
+    <>
+      <LandingPage dictionary={dictionary} />
+    </>
+
+  );
+}
